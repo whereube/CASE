75e1c1b4a1e2f633b5d8b4832db6227d80273c6a
whereube <100147393+whereube@users.noreply.github.com>
2022-09-16 10:49:10 +0200

    start

---
+++ database.js
@@ -0,0 +1,20 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: 'localhost',
+    user: 'teodor',
+    port: 5432,
+    password: 'freebird',
+    database: 'carshop'
+})
+
+client.connect();
+
+client.query('select * from employees', (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+})
